aa9f12a3045fea65388d00a70bc6bc5e5f344ede
Joshua Sulkers <josh.sulkers@gmail.com>
2016-04-26 23:09:53 -0700

    Add css

--- reader.rb
+++ reader.rb
@@ -5,12 +5,6 @@ file = File.read('stocks.json')
 
 @company_json = JSON.parse(file)
 
-# puts data_hash
-
-# data_hash["Companies"].each do |company|
-# 	puts company["name"]
-# end
-
 template_file = File.open("template.html.erb", 'r').read
 erb = ERB.new(template_file)
 
